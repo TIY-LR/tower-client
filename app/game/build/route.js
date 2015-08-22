f8c2b6641ad291ade15c669eb0365eac48063e51
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-22 13:52:43 -0500

    Fixed conflicts

--- app/game/build/route.js
+++ app/game/build/route.js
@@ -1,13 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-<<<<<<< HEAD
+
   model:function() {
     // Create a new menu item instance
     // that we'll save to the server.
     return this.store.createRecord('structure');
   },
-=======
-
->>>>>>> 708c93fdf402591b6c0dd69081024ca71fd2aa54
 });
--- app/models/apartment.js
+++ app/models/apartment.js
@@ -3,13 +3,11 @@
 // Initial Cost (how much does it cost to buy this floor?)
 // Supported Population (how many people are allowed on this floor?)
 
+import Ember from 'ember';
 import Structure from './structure';
 import DS from 'ember-data';
 
-<<<<<<< HEAD
 export default Structure.extend({
-=======
-export default DS.Model.extend({
   upkeep: DS.attr('number'),
   initialCost: DS.attr('number'),
   supportedPop: DS.attr('number'),
@@ -17,5 +15,4 @@ export default DS.Model.extend({
   image: Ember.computed('name', function() {
     return `${this.get('name')}.gif`;
   })
->>>>>>> 708c93fdf402591b6c0dd69081024ca71fd2aa54
 });
