e7ceb8e1f6d3f564307c15ca5ada59e0ad028c27
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-22 10:49:54 -0500

    Created the apartment, business and game models

---
+++ app/models/apartment.js
@@ -0,0 +1,12 @@
+// Apartment Model
+// Upkeep (cost to maintain the apartment floor)
+// Initial Cost (how much does it cost to buy this floor?)
+// Supported Population (how many people are allowed on this floor?)
+
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  upkeep: DS.attr('number'),
+  initialCost: DS.attr('number'),
+  supportedPop: DS.attr('number'),
+});
